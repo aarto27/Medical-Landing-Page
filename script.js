25f6c82ca67b284456875becfbb6b02f830c237a
Sanchit <142142945+aarto27@users.noreply.github.com>
2025-03-12 16:20:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -64,3 +64,20 @@ links.forEach((link) => {
         link.classList.remove("active");
     }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("show");
+          } else {
+              entry.target.classList.remove("show"); 
+          }
+      });
+  }, { threshold: 0.1 });
+
+  document.querySelectorAll(".box").forEach(element => {
+      observer.observe(element);
+  });
+});
+
