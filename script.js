1d86c6dd6a75f44f64da7ac7ebb7f52897fa8703
Sanchit <142142945+aarto27@users.noreply.github.com>
2025-03-12 12:18:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 document.getElementById("Appointment").addEventListener("click", function(){
     window.location.href = "contact.html";
-})
+});
 
-document.getElementById("send").addEventListener("click", function(){
-    alert('Your data has been submitted ')
-})
+document.getElementById("seeDoc").addEventListener("click", function(){
+  window.location.href = "doctor.html";
+});
+
+
+// Form Submit 
+document.getElementById("send").addEventListener("click", function() {
+  
+  document.getElementById("appointmentForm").requestSubmit();
+});
+
+document.getElementById("appointmentForm").addEventListener("submit", function(e) {
+  e.preventDefault(); 
+  let firstName = document.getElementById("name").value;
+  let lastName = document.getElementById("lname").value;
+  let email = document.getElementById("Email").value;
+  let phone = document.getElementById("number").value;
+  let appointmentDate = document.getElementById("date").value;
+  let appointmentTime = document.getElementById("time").value;
+  let description = document.getElementById("description").value;
+
+  console.log("First Name:", firstName);
+  console.log("Last Name:", lastName);
+  console.log("Email:", email);
+  console.log("Phone:", phone);
+  console.log("Appointment Date:", appointmentDate);
+  console.log("Appointment Time:", appointmentTime);
+  console.log("Description:", description);
+
+  alert("Form submitted! Check the console for details.");
+});
 
 
 // JS for PopUp
@@ -19,4 +47,20 @@ popBtn.addEventListener("click", function(){
 
 close.addEventListener("click", function(){
   popUp.style.display = "none";
-  });
+});
+
+
+let currentPage = window.location.pathname.split("/").pop();
+
+const links = document.querySelectorAll(".doc-header-sec a");
+
+links.forEach((link) => {
+    let linkPath = link.getAttribute("href");
+
+   
+    if (linkPath === currentPage) {
+        link.classList.add("active");
+    } else {
+        link.classList.remove("active");
+    }
+});
